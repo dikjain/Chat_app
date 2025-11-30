f2e7e3c7b0fece2c031ec912805ced2be4d199e8
Dikshit Mahanot <168990083+dikjain@users.noreply.github.com>
2025-11-30 20:57:16 +0530

    refactor: add file extensions to import statements for consistency

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
 import { createRoot } from 'react-dom/client'
-import App from '@/App.jsx'
+import App from '@/App'
 import '@/index.css'
 import 'react-chat-elements/dist/main.css'
 import { BrowserRouter } from 'react-router-dom'
-import ErrorBoundary from '@/components/ErrorBoundary/ErrorBoundary.jsx'
+import ErrorBoundary from '@/components/ErrorBoundary/ErrorBoundary'
 import { Toaster } from '@/components/UI/sonner'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
