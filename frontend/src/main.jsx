199c567d47aafab049c5bd118edeab37804ee889
Dikshit Mahanot <168990083+dikjain@users.noreply.github.com>
2025-11-30 21:01:33 +0530

    refactor: standardize import statements by adding file extensions for clarity

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
 import { createRoot } from 'react-dom/client'
-import App from '@/App'
+import App from '@/App.jsx'
 import '@/index.css'
 import 'react-chat-elements/dist/main.css'
 import { BrowserRouter } from 'react-router-dom'
-import ErrorBoundary from '@/components/ErrorBoundary/ErrorBoundary'
+import ErrorBoundary from '@/components/ErrorBoundary/ErrorBoundary.jsx'
 import { Toaster } from '@/components/UI/sonner'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
